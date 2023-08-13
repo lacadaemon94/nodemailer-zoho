97ebc08ad9c55ca22a2ae419bd3cf6c74b4566ef
Javier Flores <famflores10@msn.com>
2023-08-13 01:55:21 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,11 +11,7 @@ const corsHeaders = [
   },
   {
     key: "Access-Control-Allow-Headers",
-    value: "Content-Type, Authorization, x-localhost-origin",
-  },
-  {
-    key: "Access-Control-Allow-Credentials",
-    value: "true",
+    value: "Content-Type, X-Auth-Token, Origin",
   },
 ];
 
@@ -23,34 +19,7 @@ async function headers() {
   return [
     {
       source: "/(.*)",
-      headers: [
-        ...corsHeaders,
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-        },
-        {
-          key: "X-Frame-Options",
-          value: "DENY",
-        },
-        {
-          key: "X-XSS-Protection",
-          value: "1; mode=block",
-        },
-        {
-          key: "Referrer-Policy",
-          value: "strict-origin-when-cross-origin",
-        },
-        {
-          key: "Permissions-Policy",
-          value: "geolocation=(), microphone=(), camera=()",
-        },
-        {
-          key: "Content-Security-Policy",
-          value:
-            "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://www.gstatic.com; img-src 'self' https://translate.google.com https://translate.googleapis.com https://www.google.com https://fonts.gstatic.com https://www.gstatic.com data:; connect-src https://translate.googleapis.com 'self'; manifest-src 'self'; script-src-elem 'self' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com",
-        },
-      ],
+      headers: [...corsHeaders],
     },
   ];
 }
