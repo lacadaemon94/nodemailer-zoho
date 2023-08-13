1647224966b7e809ae9e8b136461d5c9c6dfb35a
Javier Flores <famflores10@msn.com>
2023-08-13 02:21:07 +0000

    Update mailConfig.ts

--- app/utils/mailConfig.ts
+++ app/utils/mailConfig.ts
@@ -3,8 +3,9 @@ import nodemailer from "nodemailer";
 const { NEXT_PUBLIC_ZOHO_USER, NEXT_PUBLIC_ZOHO_PASSWORD } = process.env;
 
 export const transporter = nodemailer.createTransport({
-  host: "smtppro.zoho.in",
+  host: "smtppro.zoho.com",
   port: 465,
+  secure: true,
   auth: {
     user: NEXT_PUBLIC_ZOHO_USER,
     pass: NEXT_PUBLIC_ZOHO_PASSWORD,
